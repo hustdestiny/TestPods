c1df0b0efc9fdda858dfc57868485d3ff90ffc14
周欣 <zhouxin1@corp.netease.com>
2019-01-11 10:56:02 +0800

    add file

---
+++ TestTools/ZXLogTool.h
@@ -0,0 +1,19 @@
+//
+//  ZXLogTool.h
+//  TestPods
+//
+//  Created by zhouxin on 2019/1/11.
+//  Copyright © 2019 zhou. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ZXLogTool : NSObject
+
++ (void)log: (NSString *)text;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ TestTools/ZXLogTool.m
@@ -0,0 +1,17 @@
+//
+//  ZXLogTool.m
+//  TestPods
+//
+//  Created by zhouxin on 2019/1/11.
+//  Copyright © 2019 zhou. All rights reserved.
+//
+
+#import "ZXLogTool.h"
+
+@implementation ZXLogTool
+
++ (void)log: (NSString *)text {
+    NSLog(@"%@", text);
+}
+
+@end
