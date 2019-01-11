a68eb65bffb3795997a9ab00086aa433a1ecfdaa
周欣 <zhouxin1@corp.netease.com>
2019-01-11 10:44:13 +0800

    Initial Commit

---
+++ TestPods/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestPods
+//
+//  Created by zhouxin on 2019/1/11.
+//  Copyright © 2019 zhou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestPods/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestPods
+//
+//  Created by zhouxin on 2019/1/11.
+//  Copyright © 2019 zhou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
